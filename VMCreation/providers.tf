d999a6e0f4f62f8c03443980f5aa88b122fb0d69
Balla Phanindra <106255195+BallaPhanindra@users.noreply.github.com>
2025-11-16 18:30:10 +0530

    Add files via upload

---
+++ VMCreation/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+  subscription_id="163d1a9e-39a3-47c2-a3a4-4633f5ea5c7f"
+}

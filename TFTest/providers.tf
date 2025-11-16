bae48e558cad6b4367d8b618562efa069a85de33
Balla Phanindra <106255195+BallaPhanindra@users.noreply.github.com>
2025-11-16 18:38:58 +0530

    Add files via upload
    Uploading Terraform code.

---
+++ TFTest/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id   =   "ca9f7c11-daf0-4b0f-9e26-133237f0848f"
+}
